ed0d029082515f0cf3f9f12297c3ac4fca2d4b81
MillzyDev <64277238+MillzyDev@users.noreply.github.com>
2021-07-05 23:02:13 +0100

    Update .gitignore

--- Java/target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"dev.MillzyG.MillzyLogger","l":"Logger.Level"},{"p":"dev.MillzyG.MillzyLogger","l":"Logger"}];updateSearchResults();
